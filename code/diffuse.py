08433d86a03d02cef8880b3948e923b4ae34e78f
Danny Goldstein <dgold@berkeley.edu>
2014-10-31 17:33:20 -0700

    documented diffuse

--- code/diffuse.py
+++ code/diffuse.py
@@ -3,7 +3,25 @@
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 
-def diffuse(array, **kwargs):
+def diffuse(d, **kwargs):
+    '''Uses the pair-wise distance matrix for a data set to compute
+    the diffusion map coefficients. Computes teh Markov transition
+    probability matrix, and its eigenvalues and left and reight
+    eigenvectors. Returns a `robjects.r.dmap` object.
+    
+    Parameters:
+    -----------
+    d: numpy.ndarray with shape (n_samples, n_samples).  n-by-n
+        pairwise distance matrix for a data set with n points or
+        alternatively the output from the rpy2.robjects.r.dist
+        function.
+    
+    Returns:
+    --------
+    rpy2.robjects.r.dmap object containing the diffusion map
+        coefficients for each sample.
+    '''
+
     
     dM=importr('diffusionMap')
     nr, nc = array.shape

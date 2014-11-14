d32eacf2e368b66f084c667e145c499b2d575933
Alex <agkim@lbl.gov>
2014-11-13 17:18:53 -0800

    add python implementation of diffuse

--- src/diffuse.py
+++ src/diffuse.py
@@ -13,6 +13,9 @@ rpy2.robjects.numpy2ri.activate()
 import numpy
 import numpy as np
 import gc
+import scipy
+from scipy.stats import norm
+from scipy.sparse import *
 
 def diffuse(d, **kwargs):
     '''Uses the pair-wise distance matrix for a data set to compute
@@ -30,19 +33,34 @@ def diffuse(d, **kwargs):
     rpy2.robjects.r.dmap object containing the diffusion map
         coefficients for each sample.
     '''
-
     dM=importr('diffusionMap')
-    xr=robjects.r.dist(d)
 
-    # must be better way to do this but convert returned lower triangualr
-    # array into an array
-    xr_arr=np.zeros((d.shape[0],d.shape[0]))
-    for i in xrange(0,d.shape[0]-1):
-      for j in xrange(i+1,d.shape[0]):
-        xr_arr[i,j]=xr[d.shape[0]*i - i*(i+1)/2 + j-i -1]
-        xr_arr[j,i]=xr_arr[i,j]
-    dmap = dM.diffuse(xr_arr, **kwargs)
-    
+    # R distance
+    #xr=numpy.array(robjects.r.dist(d))
+    # Python distance
+    xr = scipy.spatial.distance.pdist(d, 'euclidean')
+#    file = open('dist.txt','w')
+#    for x in xr:
+#      file.write(str(x)+'\n')
+#    file.close()
+#    print d.shape, xr.shape
+#    fwf
+#    print 'distance',xr.min(),xr.max()
+    xr_arr=scipy.spatial.distance.squareform(xr)
+    #kwargs['eps.val']=2*numpy.sort(xr)[10]**2 #dM.epsilonCompute(xr_arr,p=5e-3)[0]
+#    kwargs['eps_val']=dM.epsilonCompute(xr_arr,p=5e-3)[0]
+#    kwargs['neigen']=10
+#    kwargs['maxdim']=100
+#    kwargs['t']=0
+#    print kwargs
+ 
+#    dmap = dM.diffuse(xr_arr, **kwargs)
+#    print 'R', numpy.amin(dmap.rx('eigenvals')[0]), numpy.amax(dmap.rx('eigenvals')[0])
+#    print 'R', numpy.amin(dmap.rx('psi')[0]), numpy.amax(dmap.rx('psi')[0])
+#    print 'R', numpy.amin(dmap.rx('phi')[0]), numpy.amax(dmap.rx('phi')[0])
+#    print 'R', numpy.amin(dmap.rx('X')[0]), numpy.amax(dmap.rx('X')[0])
+    dmap = diffuse_py(xr_arr, **kwargs)
+    print dmap['X'].min(), dmap['X'].min()
     # garbage collection
     gc.collect()
 
@@ -98,8 +116,12 @@ def nystrom(dmap, orig, d, sigma='default'):
     # represents distances from new data to original data
     distmat_slice = distmat[len(orig):, :len(orig)]
     # Compute nystrom and return
-    #coords = dM.nystrom(dmap, distmat_slice, **kwargs)
-    coords = nystrom_py(dmap, distmat_slice, **kwargs)
+
+    # R version
+    coords = dM.nystrom(dmap, distmat_slice, **kwargs)
+    # Python version
+    #coords = nystrom_py(dmap, distmat_slice, **kwargs)
+
     # python garbage collection
     gc.collect()
     return coords
@@ -142,3 +164,85 @@ def nystrom_py(dmap, Dnew, **kwargs):
 #    print 'Xnew', Xnew.shape, Xnew
 #    Xnew_w = numpy.dot(Xnew_w ,numpy.dot( X , emat))
     return Xnew
+
+def epsilonCompute(D, p=0.01):
+
+#   D=scipy.spatial.distance.squareform(D)
+   n = D.shape[0]
+   k = numpy.ceil(p*n)
+   k = numpy.maximum(2,k)
+   D_sort = numpy.sort(D,0)
+   dist_knn = D_sort[k,:]
+   epsilon = 2*numpy.median(dist_knn)**2
+   return epsilon
+#epsilonCompute <- function(D,p=.01){
+
+#  D = as.matrix(D)
+#  n = dim(D)[1]
+#  k = ceiling(p*n)
+#  k = ifelse(k<2,2,k) # use k of at least 2
+#  D.sort = apply(D,1,sort)
+#  dist.knn = D.sort[(k+1),] # find dists. to kth nearest neighbor
+#  epsilon = 2*median(dist.knn)^2
+#
+#  return(epsilon)
+#}
+
+
+#diffuse <- function(D,eps.val=epsilonCompute(D),neigen=NULL,t=0,maxdim=50,delta=10^-5) {
+def diffuse_py(D,eps_val='default',neigen=None,t=0,maxdim=50,delta=1e-5):
+  if eps_val == 'default':
+    eps_val = epsilonCompute(D)
+
+  n=D.shape[0]
+  K=numpy.exp(-D**2/eps_val)
+  v = numpy.sqrt(numpy.sum(K,axis=0))
+  
+  A= K / numpy.outer(v,v)
+  w=numpy.where(A > delta)
+  Asp =  csc_matrix( (A[w],(w[0],w[1])), shape=A.shape )
+
+  if neigen is None:
+    neff = numpy.minimum(maxdim,n)
+  else:
+    neff =  numpy.minimum(neigen, n)
+
+  from scipy.sparse.linalg import eigsh
+  evals, evecs = eigsh(Asp, k=neff, which='LA', ncv=numpy.maximum(30,2*neff))
+#  evecs= numpy.array([[1.,4,7],[2,5,8],[3,6,9],[5,10,15]]) 
+  #evecs = matrix(c(1,2,3,5,4,5,6,10,7,8,9,15),nrow=4,ncol=3)
+  psi = evecs[:,neff-1::-1]/numpy.outer(evecs[:,neff-1],numpy.zeros(neff)+1)
+  phi = evecs[:,neff-1::-1]*numpy.outer(evecs[:,neff-1],numpy.zeros(neff)+1)
+
+  eigenvals = evals[neff::-1]#eigenvalues
+
+  if t<=0:
+    lambd = eigenvals[1:]/(1-eigenvals[1:])
+    lambd= numpy.outer(numpy.zeros(n)+1., lambd)
+    if neigen is None:
+      lam=lambd[0,:]/lambd[0,0]
+      neigen= numpy.sum(lam < 0.05)
+      neigen = numpy.minimum(neigen,maxdim)
+      eigenvals = eigenvals[0:neigen]
+    X = psi[:,1:neigen+1]*lambd[:,0:neigen]
+  else:
+    lambd= eigenvals[1:]**t
+    lambd=numpy.outer(numpy.zeros(n)+1., lambd)
+    if neigen is None:
+      lam=lambd[0,:]/lambd[0,0]
+      neigen= numpy.sum(lam < 0.05)
+      neigen = numpy.minimum(neigen,maxdim)
+      eigenvals = eigenvals[0:neigen]
+    X = psi[:,1:neigen+1]*lambd[:,0:neigen]
+
+  y=dict()
+  y['X']=X
+  y['phi0']=phi[:,0]
+  y['eigenvals']=eigenvals[1:]
+  y['eigenmult']=lambd[:,:neigen]
+  y['psi']=psi
+  y['phi']=phi
+  y['neigen']=neigen
+  y['epsilon']=eps_val
+
+  return y
